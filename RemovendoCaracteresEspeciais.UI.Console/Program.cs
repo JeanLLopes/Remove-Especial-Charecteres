80ca994ad4a7d909c3ea2b8391de6827c0fc56e5
JeanLLopes <jean.llopes@outlook.com>
2015-06-09 00:38:14 -0300

    InitialCommit

---
+++ RemovendoCaracteresEspeciais.UI.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovendoCaracteresEspeciais.UI.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Escreva um nome com acento, para que possamos retirar os acentos: ");
+            var userTextScreen = System.Console.ReadLine();
+            var result = new RemoveCharacteres().RemoveEspecialCharecteres(userTextScreen);
+            System.Console.WriteLine("Você digitou o seguinte texto sem acento: "+ result);
+            System.Console.ReadLine();
+        }
+    }
+}
