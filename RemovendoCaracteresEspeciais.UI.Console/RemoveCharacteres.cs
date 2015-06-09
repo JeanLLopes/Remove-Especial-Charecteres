1de9436e8dbdb0e04d8fdc21da042d5f585fb294
JeanLLopes <jean.llopes@outlook.com>
2015-06-09 00:39:12 -0300

    Alter-Changes

---
+++ RemovendoCaracteresEspeciais.UI.Console/RemoveCharacteres.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RemovendoCaracteresEspeciais.UI.Console
+{
+    public sealed class RemoveCharacteres
+    {
+        public String RemoveEspecialCharecteres(String text)
+        {
+            var returnText = new StringBuilder();
+            var arrayText = text.Normalize(NormalizationForm.FormKD).ToCharArray();
+
+
+            foreach (var textSeparate in arrayText)
+            {
+                if (Regex.IsMatch(Convert.ToString(textSeparate), "[a-zA-Z0-9]"))
+                {
+                    returnText.Append(textSeparate);
+                }
+
+            }
+
+            return returnText.ToString();
+        }
+    }
+}
